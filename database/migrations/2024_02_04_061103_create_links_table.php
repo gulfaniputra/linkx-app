ee235da4b4d504d552235c5045db78fe32015f77
Gulfani Putra <gulfaniputra@gmail.com>
2024-02-04 14:36:29 +0800

    Configure migrations tables' columns per ERD

--- database/migrations/2024_02_04_061103_create_links_table.php
+++ database/migrations/2024_02_04_061103_create_links_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('links', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->string('name');
+            $table->text('link');
             $table->timestamps();
         });
     }
--- database/migrations/2024_02_04_061211_create_visits_table.php
+++ database/migrations/2024_02_04_061211_create_visits_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('visits', function (Blueprint $table) {
             $table->id();
+            $table->integer('link_id');
+            $table->text('user_agent')->nullable();
             $table->timestamps();
         });
     }
