f347f62eaec23119182b24e9694144c5fe177eb4
Gulfani Putra <gulfaniputra@gmail.com>
2024-02-05 16:46:42 +0800

    Configure routes & authentication

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,35 @@
 <?php
 
+use App\Http\Controllers\LinkController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\VisitController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Auth::routes();
+
+// http://127.0.0.1:8000/dashboard/
+Route::group(['middleware' => 'auth', 'prefix' => 'dashboard'], function () {
+
+    route::get('/links', [LinkController::class, 'index']);
+    route::get('/links/new', [LinkController::class, 'create']);
+    route::post('/links/new', [LinkController::class, 'store']);
+    route::get('/links/{link}', [LinkController::class, 'edit']);
+    route::post('/links/{link}', [LinkController::class, 'update']);
+    route::delete('/links/{link}', [LinkController::class, 'destroy']);
+
+    Route::get('/settings', [UserController::class, 'edit']);
+    Route::post('/settings', [UserController::class, 'update']);
+});
+
+Route::post('/visit/{link}', [VisitController::class, 'store']);
+
+// http://127.0.0.1:8000/username
+Route::get('/{user}', [UserController::class, 'show']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
